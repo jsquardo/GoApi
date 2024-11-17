6e025b18416ea38e42dfd17066079f7c6552e94a
Johnny Squardo <johnnysquardo@gmail.com>
2024-11-17 12:21:11 -0500

    Added middleware authorization.go

--- internal/middleware/authorization.go
+++ internal/middleware/authorization.go
@@ -1,1 +1,23 @@
 package middleware
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/avukadin/goapi/api"
+	"github.com/avukadin/goapi/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
+
+var UnAuthorizedError = errors.new("Invalid username or token.")
+
+func Authorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		var username = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+
+	})
+}
